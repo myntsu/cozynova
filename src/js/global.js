0a974ec23b40870807951d2275885d7488e481ac
Myntsu <dingodille16@hotmail.com>
2022-11-04 17:35:03 -0300

    minor fixes

---
+++ src/js/global.js
@@ -0,0 +1,27 @@
+const colorThemes = document.querySelectorAll('[name="theme"]');
+
+// store theme
+const storeTheme = function (theme) {
+  localStorage.setItem("theme", theme);
+};
+
+// set theme when visitor returns
+const setTheme = function () {
+  const activeTheme = localStorage.getItem("theme");
+  colorThemes.forEach((themeOption) => {
+    if (themeOption.id === activeTheme) {
+      themeOption.checked = true;
+    }
+  });
+  // fallback for no :has() support
+  document.documentElement.className = activeTheme;
+};
+
+colorThemes.forEach((themeOption) => {
+  themeOption.addEventListener("input", () => {
+    storeTheme(themeOption.id);
+    document.documentElement.className = themeOption.id
+  });
+});
+  
+document.DOMDocumentLoad = setTheme();
