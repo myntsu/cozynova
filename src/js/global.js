0fbb88b3864ddf47fd6d04a2e210b1c97078fadf
Myntsu <dingodille16@hotmail.com>
2022-11-16 23:20:31 -0300

    adding cookies policy

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,15 @@
+// Bootstrap
+// Enabling popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+// Enabling tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+// Color theme changer
 const colorThemes = document.querySelectorAll('[name="theme"]');
 
 // store theme
@@ -24,5 +36,20 @@ colorThemes.forEach((themeOption) => {
     document.documentElement.className = themeOption.id;
   });
 });
-  
+
 document.DOMDocumentLoad = setTheme();
+
+// Cookie consent
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-button");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active-cookie");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed", "true")) {
+    cookieContainer.classList.add("active-cookie");
+  }
+}, 2000);
