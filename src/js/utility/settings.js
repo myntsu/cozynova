b291d0e69e9c09086358bea7f3ee103763a4c109
Myntsu <dingodille16@hotmail.com>
2023-08-01 19:15:16 -0400

    Added all the features to the live version 1.0

--- src/js/utility/settings.js
+++ src/js/utility/settings.js
@@ -1,33 +1,37 @@
-function handleClick() {
-    settings.classList.toggle("gear-active");
-  
-    // Start the animation when the gear-active class is added
-    if (settings.classList.contains("gear-active")) {
-      handleInteraction();
-    }
+function handleClick(event) {
+  event.preventDefault();
+  console.log("handleClick called");
+  settings.classList.toggle("gear-active");
+
+  // Start the animation when the gear-active class is added
+  if (settings.classList.contains("gear-active")) {
+    handleInteraction();
   }
+}
 
 const settings = document.querySelector(".fa-gear");
 
 function handleInteraction() {
-    settings.classList.add("settings-animation");
-    settings.addEventListener("animationend", () => {
-      settings.classList.remove("settings-animation");
-    });
-  }
+  settings.classList.add("settings-animation");
+  settings.addEventListener("animationend", () => {
+    settings.classList.remove("settings-animation");
+  });
+}
 
-  function handleDocumentClick(event) {
-    const placeholder = document.getElementById("placeholder");
-  
-    if (
-      !settings.contains(event.target) &&
-      (!placeholder || !placeholder.contains(event.target))
-    ) {
+function handleDocumentClick(event) {
+  const placeholder = document.getElementById("placeholder");
+
+  if (
+    !settings.contains(event.target) &&
+    (!placeholder || !placeholder.contains(event.target))
+  ) {
+    setTimeout(() => {
       settings.classList.remove("gear-active");
-    }
+    }, 300);
   }
+}
 
-  settings.addEventListener("click", handleClick);
-  settings.addEventListener("touchstart", handleClick);
-  document.addEventListener("click", handleDocumentClick);
-  document.addEventListener("touchstart", handleDocumentClick);
+settings.addEventListener("click", handleClick);
+settings.addEventListener("touchend", handleClick);
+document.addEventListener("click", handleDocumentClick);
+document.addEventListener("touchend", handleDocumentClick);
